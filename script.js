e9f43a152c690906274b71bffc82ec911ddbf3ed
esakkiappan <eappan241@gmail.com>
2026-06-22 09:37:35 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,103 @@
+//simple calculator
+let num1=6;
+let num2=0;
+
+let operator="/"; 
+
+if(operator==="+"){
+    console.log(num1+num2);
+    
+}
+else if(operator==="-"){
+    console.log(num1-num2);
+
+}
+else if(operator==="*"){
+    console.log(num1*num2);
+}
+else if(operator==="/");
+  if (num2!=0){
+    console.log("num2 can be zero");    
+}
+ else{
+    console.log("num2 cannot be zero");
+    
+} 
+
+// Create a variable `marks` to store student marks (0–100).
+
+
+// Add bonus marks using an assignment operator (+=).
+
+
+// Determine the grade using if-else:
+// 90–100 → Grade A
+// 70–89 → Grade B
+// 50–69 → Grade C
+// Below 50 → Grade F
+
+
+// Determine Pass/Fail using a ternary operator:
+// (marks >= 50 ? "Pass" : "Fail")
+
+
+// Give remarks based on grade using a switch statement:
+// A → "Excellent!"
+// B → "Good"
+// C → "Average"
+// F → "Needs Improvement"
+
+
+// Print the results in the console:
+// Marks, Grade, Result, Remark
+
+let mark = 90;
+let bonus = 5;
+
+mark += bonus;
+
+let grade;
+
+if (mark >= 90 && mark <= 100) {
+    grade = "A";
+}
+else if (mark >= 70 && mark <= 89) {
+    grade = "B";
+}
+else if (mark >= 50 && mark <= 69) {
+    grade = "C";
+}
+else {
+    grade = "F";
+}
+
+let result = (mark >= 50) ? "Pass" : "Fail";
+
+let remark;
+
+switch (grade) {
+    case "A":
+        remark = "Excellent!";
+        break;
+
+    case "B":
+        remark = "Good";
+        break;
+
+    case "C":
+        remark = "Average";
+        break;
+
+    case "F":
+        remark = "Needs Improvement";
+        break;
+
+    default:
+        remark = "Invalid Grade";
+}
+
+console.log("Marks:", mark);
+console.log("Grade:", grade);
+console.log("Result:", result);
+console.log("Remark:", remark);
+
